22c81dd11ff1d0b70a9ea5756177542b2bd9e953
Rodrigopca42 <rodrigopca426281@gmail.com>
2026-05-02 17:47:43 -0300

    primeiros comandos de teste no kanban

---
+++ web/cypress/e2e/kanban.cy.js
@@ -0,0 +1,8 @@
+describe('Kanban Board', () => {
+    it('Deve mover uma tarefa de Todo paa Done e atualizar o board', () => {
+        cy.startDesktop()
+        cy.submitLogin('papito@webdojo.com', 'katana123')
+
+        cy.contains('Kanban').click()
+    })
+})
